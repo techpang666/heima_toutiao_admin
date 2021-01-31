d77078683ade0bfaf0f12a48f4f57a19e01a80fb
TECHPANG_dev <TECHPANG_dev@test.com>
2021-02-01 07:27:30 +0800

    finish_loginStyle_add_handlerFocus

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,26 +1,32 @@
 <template>
 	<div class="login">
-		<el-form
-			:model="loginForm"
-			:rules="rules"
-			ref="ruleForm"
-			class="demo-ruleForm"
-		>
-			<el-form-item prop="username">
-				<el-input
-					prefix-icon="icon-user"
-					placeholder="请输入用户名"
-					v-model="loginForm.username"
-				></el-input>
-			</el-form-item>
-			<el-form-item prop="password">
-				<el-input
-					prefix-icon="icon-key"
-					placeholder="请输入密码"
-					v-model="loginForm.password"
-				></el-input>
-			</el-form-item>
-		</el-form>
+		<div class="container">
+			<img src="../assets/avatar.jpg" class="avatar" alt="" />
+			<el-form
+				:model="loginForm"
+				:rules="rules"
+				ref="loginForm"
+				class="demo-ruleForm"
+			>
+				<el-form-item prop="username">
+					<el-input
+						@focus="handlerFocus('username')"
+						prefix-icon="icon-user"
+						placeholder="请输入用户名"
+						v-model="loginForm.username"
+					></el-input>
+				</el-form-item>
+				<el-form-item prop="password">
+					<el-input
+						@focus="handlerFocus('password')"
+						prefix-icon="icon-key"
+						placeholder="请输入密码"
+						v-model="loginForm.password"
+					></el-input>
+				</el-form-item>
+				<el-button type="primary" class="login-btn">登陆</el-button>
+			</el-form>
+		</div>
 	</div>
 </template>
 
@@ -46,7 +52,48 @@ export default {
 			},
 		};
 	},
+	methods: {
+		// 处理输入框的焦点事件
+		handlerFocus(propName) {
+			// 移出表单项的验证结果(clearValidate)
+			// 如果不传参会移出整个表单结果(propName)
+			this.$refs.loginForm.clearValidate(propName);
+		},
+	},
 };
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.login {
+	position: fixed;
+	width: 100%;
+	height: 100%;
+	background-color: #2f4050;
+
+	.container {
+		position: absolute;
+		left: 0;
+		right: 0;
+		width: 400px;
+		padding: 0px 40px 15px 40px;
+		margin: 200px auto;
+		background: white;
+		.avatar {
+			position: relative;
+			left: 50%;
+			width: 120px;
+			height: 120px;
+			margin-left: -60px;
+			margin-top: -60px;
+			box-sizing: border-box;
+			border-radius: 50%;
+			border: 10px solid #fff;
+			box-shadow: 0 1px 5px #ccc;
+			overflow: hidden;
+		}
+		.login-btn {
+			width: 100%;
+		}
+	}
+}
+</style>
