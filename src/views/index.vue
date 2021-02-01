d74c011fbaa92d15fc3a5044e06d2a0178fd5114
TECHPANG_dev <TECHPANG_dev@test.com>
2021-02-02 02:09:16 +0800

    finish_navMenu_static

--- src/views/index.vue
+++ src/views/index.vue
@@ -8,6 +8,8 @@
 					background-color="#545c64"
 					text-color="#fff"
 					active-text-color="#ffd04b"
+					:unique-opened="true"
+					:router="true"
 				>
 					<!-- 用户管理 -->
 					<el-submenu index="1">
@@ -21,27 +23,27 @@
 						</el-menu-item>
 					</el-submenu>
 					<!-- 文章管理 -->
-					<el-submenu index="1">
+					<el-submenu index="2">
 						<template slot="title">
 							<i class="el-icon-location"></i>
 							<span>文章管理</span>
 						</template>
-						<el-menu-item index="1-1">
+						<el-menu-item index="2-1">
 							<i class="el-icon-user"></i>
 							<span>文章列表</span>
 						</el-menu-item>
-						<el-menu-item index="1-1">
+						<el-menu-item index="2-2">
 							<i class="el-icon-user"></i>
 							<span>文章发布</span>
 						</el-menu-item>
 					</el-submenu>
 					<!-- 栏目管理 -->
-					<el-submenu index="1">
+					<el-submenu index="3">
 						<template slot="title">
 							<i class="el-icon-location"></i>
 							<span>栏目管理</span>
 						</template>
-						<el-menu-item index="1-1">
+						<el-menu-item index="3-1">
 							<i class="el-icon-user"></i>
 							<span>栏目列表</span>
 						</el-menu-item>
